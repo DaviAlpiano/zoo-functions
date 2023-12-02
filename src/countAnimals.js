126c9bd4c2696df763659453277018564d0a6f0f
Davi Alpiano <davialpiano@gmail.com>
2023-12-02 19:16:35 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const { species } = data;
+  if (animal === undefined) {
+    return species.reduce((acc, value) => {
+      acc[value.name] = value.residents.length;
+      return acc;
+    }, {});
+  } if ('sex' in animal) {
+    return species.find((specie) => specie.name === animal.species)
+      .residents
+      .filter((resid) => animal.sex === resid.sex).length;
+  } return species.find((specie) => specie.name === animal.species)
+    .residents.length;
 };
-
+console.log(countAnimals({ species: 'giraffes' }));
 module.exports = countAnimals;
