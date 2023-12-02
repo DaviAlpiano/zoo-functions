8edb4da85ccfc02a7f708c088d379ecb394f6c9b
Davi Alpiano <davialpiano@gmail.com>
2023-12-02 14:57:06 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
+  const { employees } = data;
 
+  const array = employees.filter((employee) => {
+    if (employee.firstName === employeeName || employee.lastName === employeeName) {
+      return employee;
+    }
+    return undefined;
+  });
+
+  if (array[0] === undefined) {
+    return {};
+  } return array[0];
+};
+console.log(getEmployeeByName('Nelson'));
 module.exports = getEmployeeByName;
