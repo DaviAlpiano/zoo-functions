9b38778f3882e69b9d45f40446e5df782072fbe3
Davi Alpiano <davialpiano@gmail.com>
2023-12-02 17:30:15 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const genrente = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+    '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
+  return genrente.some((employee) => employee === id);
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  const { employees } = data;
+  isManager(managerId);
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } return employees.filter((employee) => (employee.managers.includes(managerId)))
+    .map((dado) => `${dado.firstName} ${dado.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
