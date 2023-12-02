4f8749e867c6fd39b0a0d08556782f23c9e6dcad
Davi Alpiano <davialpiano@gmail.com>
2023-12-01 21:15:05 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const { species } = data;
+  return species.filter((specie) => specie.name === animal)
+    .map((specis) => specis.residents)
+    .some((resident) => resident.every((res) => res.age >= age));
 };
-
+console.log(getAnimalsOlderThan('penguins', 2));
 module.exports = getAnimalsOlderThan;
