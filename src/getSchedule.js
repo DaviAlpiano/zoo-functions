c600b4e54fad20dacab97dc02512f12bcc4c3260
Davi Alpiano <davialpiano@gmail.com>
2023-12-02 23:20:07 -0300

    requisito 9 quase completo

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const { species, hours } = data;
+  const dias = Object.keys(hours);
+  const hora = Object.values(hours);
+  return dias.reduce((acc, actual, index) => {
+    acc[actual] = {
+      officeHour: `Open from ${hora[index].open}am until ${hora[index].close}pm`,
+      exhibition: species.filter((specie) =>
+        specie.availability.includes(actual))
+        .map((animal) => animal.name),
+    };
+    return acc;
+  }, {});
+  species.find((specie) => specie.name === scheduleTarget).availability;
 };
 
+console.log(getSchedule());
 module.exports = getSchedule;
