880e8588ac1c38f2c6f59e335d5830333a86bbc1
Davi Alpiano <davialpiano@gmail.com>
2023-12-03 17:49:50 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const { species, employees } = data;
+
+const pessoas = () => employees.reduce((acc, actual) => {
+  acc.push({
+    id: actual.id,
+    fullName: `${actual.firstName} ${actual.lastName}`,
+    species: species.filter((specie) =>
+      actual.responsibleFor.includes(specie.id))
+      .map((dados) => dados.name),
+    locations: species.filter((specie) =>
+      actual.responsibleFor.includes(specie.id))
+      .map((dados) => dados.location),
+  });
+  return acc;
+}, []);
+
+const getEmployeesCoverage = (object) => {
+  if (typeof object !== 'object') {
+    return pessoas();
+  }
+  const dado = Object.values(object)[0];
+  const pessoa = pessoas().filter((employee) => employee.fullName.includes(dado)
+  || employee.id === dado);
+  if (pessoa.length === 0) {
+    throw new Error('Informações inválidas');
+  }
+  return pessoa[0];
 };
 
+console.log(getEmployeesCoverage({ id: 'Strauss' }));
 module.exports = getEmployeesCoverage;
