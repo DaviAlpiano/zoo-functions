1bf4cb336489d860384d029df5a6d3fb7f2bffce
Davi Alpiano <davialpiano@gmail.com>
2023-12-03 15:56:25 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const fistAni = employees.find((employee) =>
+    employee.id === id).responsibleFor[0];
+
+  const dados = species.find((specie) => specie.id === fistAni)
+    .residents.reduce((acc, resident) => (acc.age > resident.age ? acc : resident));
+
+  return Object.values(dados);
 };
 
+console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 module.exports = getOldestFromFirstSpecies;
