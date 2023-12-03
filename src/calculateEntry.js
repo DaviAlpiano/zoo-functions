53d2d5c80698521ec9db3e27ab43eee2cfbdd347
Davi Alpiano <davialpiano@gmail.com>
2023-12-02 21:39:16 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => entrants.reduce((acc, entrat) => {
+  if (entrat.age < 18) {
+    acc.child += 1;
+  }
+
+  if (entrat.age >= 18 && entrat.age < 50) {
+    acc.adult += 1;
+  }
+
+  if (entrat.age >= 50) {
+    acc.senior += 1;
+  }
+
+  return acc;
+}, { child: 0, adult: 0, senior: 0 });
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  const { child, adult, senior } = data.prices;
+  if (entrants === undefined || entrants.length === 0) {
+    return 0;
+  }
+  const grupo = countEntrants(entrants);
+  const preco = (grupo.child * child) + (grupo.adult * adult) + (grupo.senior * senior);
+  return parseFloat(preco.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
